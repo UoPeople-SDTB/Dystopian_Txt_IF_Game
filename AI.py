fe1a92be2a8eb5d362396a5c9bd3f63f54e5f669
JimmyTyrrell <83917548+JimmyTyrrell@users.noreply.github.com>
2023-07-14 09:13:23 -0700

    Create AI.py
    copy from old repo

---
+++ AI.py
@@ -0,0 +1,6 @@
+from Being import Being # importing the being class from the being module
+
+
+class AI(Being):
+    def __init__(self, name, description, state):
+        super().__init__(name, description, state)
